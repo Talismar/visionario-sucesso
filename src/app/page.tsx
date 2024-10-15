0d4ca727bb850e0fa1f1fe5f5bb767f692cb7df3
Talismar Fernandes Costa <talismar788.una@gmail.com>
2024-10-15 15:11:29 -0300

    feat: add amination on home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,72 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { BookOpen, CheckCircle, ChevronRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function LandingPage() {
+  const fadeIn = {
+    initial: { opacity: 0, y: 20 },
+    animate: { opacity: 1, y: 0 },
+    transition: { duration: 120 },
+  };
+
+  const staggerChildren = {
+    animate: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <header className="bg-[#c3a540]">
-        <div className="max-w-[1488px] flex flex-col sm:flex-row justify-center sm:justify-between items-center px-4 lg:px-6 h-16 mx-auto">
-          <Link className="flex items-center justify-center" href="#">
-            <BookOpen className="h-6 w-6 text-[#101010]" />
-            <span className="ml-2 sm:text-2xl  font-bold text-[#101010]">
-              Visionário de Sucesso
-            </span>
+    <div className="flex min-h-screen w-full flex-col">
+      <motion.header
+        className="px-4 lg:px-6 h-16 flex flex-col sm:flex-row items-center justify-center sm:justify-between bg-[#c3a540]"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+      >
+        <Link className="flex items-center justify-center" href="#">
+          <BookOpen className="h-6 w-6 text-[#101010]" />
+          <span className="ml-2 sm:text-2xl font-bold text-[#101010]">
+            Visionário de Sucesso
+          </span>
+        </Link>
+        <nav className="flex gap-4 sm:gap-6">
+          <Link
+            className="text-sm font-medium hover:underline underline-offset-4 text-[#101010]"
+            href="#about"
+          >
+            Sobre
           </Link>
-          <nav className="flex gap-4 sm:gap-6">
-            <Link
-              className="text-sm font-medium hover:underline underline-offset-4 text-[#101010]"
-              href="#about"
-            >
-              Sobre
-            </Link>
-            <Link
-              className="text-sm font-medium hover:underline underline-offset-4 text-[#101010]"
-              href="#benefits"
-            >
-              Benefícios
-            </Link>
-            <Link
-              className="text-sm font-medium hover:underline underline-offset-4 text-[#101010]"
-              href="#cta"
-            >
-              Comprar
-            </Link>
-          </nav>
-        </div>
-      </header>
+          <Link
+            className="text-sm font-medium hover:underline underline-offset-4 text-[#101010]"
+            href="#benefits"
+          >
+            Benefícios
+          </Link>
+          <Link
+            className="text-sm font-medium hover:underline underline-offset-4 text-[#101010]"
+            href="#cta"
+          >
+            Comprar
+          </Link>
+        </nav>
+      </motion.header>
       <main className="flex-1">
-        <section className="w-full flex justify-center py-12 md:py-24 lg:py-32 xl:py-48 bg-[#101010]">
+        <section className="w-full flex justify-center py-12 md:py-24 lg:py-32 bg-[#101010]">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
-              <Image
-                alt="Ebook Marketing Digital"
-                className="mx-auto aspect-[1/1.5] overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
-                height="600"
-                src="/logo.png"
-                width="400"
-              />
-              <div className="flex flex-col justify-center space-y-4">
+              <motion.div
+                className="flex flex-col justify-center space-y-4"
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.6 }}
+              >
                 <div className="space-y-2">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none text-[#c3a540]">
                     Domine o Marketing Digital
@@ -72,17 +90,38 @@ export default function LandingPage() {
                     Saiba Mais
                   </Link>
                 </div>
-              </div>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.6 }}
+              >
+                <Image
+                  alt="Ebook Marketing Digital"
+                  className="mx-auto aspect-[1/1.5] overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
+                  height="600"
+                  src="/logo.png"
+                  width="400"
+                />
+              </motion.div>
             </div>
           </div>
         </section>
-
         <section
           id="about"
-          className="w-full flex justify-center py-12 md:py-24 lg:py-32 bg-[#101010]"
+          className="w-full py-12 md:py-24 lg:py-32 bg-[#101010]"
         >
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+          <motion.div
+            className="container px-4 md:px-6 mx-auto"
+            variants={staggerChildren}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+          >
+            <motion.div
+              className="flex flex-col items-center justify-between space-y-4 text-center"
+              variants={fadeIn}
+            >
               <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-[#c3a540]">
                   Conheça Vitoria Nunes
@@ -92,61 +131,68 @@ export default function LandingPage() {
                   {'"Visionário de Sucesso"'}
                 </p>
               </div>
-            </div>
-            <div className="mx-auto grid  items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
-              <Image
-                alt="Vitoria Nunes"
-                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
-                height="310"
-                src="/logo.png"
-                width="550"
-              />
-              <div className="flex flex-col justify-center space-y-4">
+            </motion.div>
+            <div className="mx-auto grid items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
+              <motion.div variants={fadeIn}>
+                <Image
+                  alt="Vitoria Nunes"
+                  className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
+                  height="310"
+                  src="/logo.png"
+                  width="550"
+                />
+              </motion.div>
+              <motion.div
+                className="flex flex-col justify-center space-y-4"
+                variants={staggerChildren}
+              >
                 <ul className="grid gap-6">
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold text-[#c3a540]">
-                        Experiência Comprovada
-                      </h3>
-                      <p className="text-gray-300">
-                        Com mais de 10 anos no mercado, Vitoria já ajudou
-                        centenas de empresas a crescerem online.
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold text-[#c3a540]">
-                        Metodologia Única
-                      </h3>
-                      <p className="text-gray-300">
-                        Seu método exclusivo combina estratégias comprovadas com
-                        as últimas tendências do mercado.
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="grid gap-1">
-                      <h3 className="text-xl font-bold text-[#c3a540]">
-                        Resultados Reais
-                      </h3>
-                      <p className="text-gray-300">
-                        Clientes de Vitoria relatam um aumento médio de 300% em
-                        suas vendas online.
-                      </p>
-                    </div>
-                  </li>
+                  {[
+                    {
+                      title: "Experiência Comprovada",
+                      description:
+                        "Com mais de 10 anos no mercado, Vitoria já ajudou centenas de empresas a crescerem online.",
+                    },
+                    {
+                      title: "Metodologia Única",
+                      description:
+                        "Seu método exclusivo combina estratégias comprovadas com as últimas tendências do mercado.",
+                    },
+                    {
+                      title: "Resultados Reais",
+                      description:
+                        "Clientes de Vitoria relatam um aumento médio de 300% em suas vendas online.",
+                    },
+                  ].map((item, index) => (
+                    <motion.li key={index} variants={fadeIn}>
+                      <div className="grid gap-1">
+                        <h3 className="text-xl font-bold text-[#c3a540]">
+                          {item.title}
+                        </h3>
+                        <p className="text-gray-300">{item.description}</p>
+                      </div>
+                    </motion.li>
+                  ))}
                 </ul>
-              </div>
+              </motion.div>
             </div>
-          </div>
+          </motion.div>
         </section>
         <section
           id="benefits"
-          className="w-full flex justify-center py-12 md:py-24 lg:py-32 bg-[#c3a540]"
+          className="w-full flex flex-col items-center justify-center py-12 md:py-24 lg:py-32 bg-[#c3a540]"
         >
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+          <motion.div
+            className="container px-4 md:px-6"
+            variants={staggerChildren}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+          >
+            <motion.div
+              className="flex flex-col items-center justify-center space-y-4 text-center"
+              variants={fadeIn}
+            >
               <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-[#101010]">
                   Benefícios do Ebook
@@ -156,8 +202,11 @@ export default function LandingPage() {
                   {'"Visionário de Sucesso"'}
                 </p>
               </div>
-            </div>
-            <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-3">
+            </motion.div>
+            <motion.div
+              className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-3"
+              variants={staggerChildren}
+            >
               {[
                 "Estratégias avançadas de SEO",
                 "Táticas de marketing de conteúdo",
@@ -166,35 +215,48 @@ export default function LandingPage() {
                 "Análise de dados eficiente",
                 "Automação de marketing",
               ].map((benefit, index) => (
-                <div
+                <motion.div
                   key={index}
                   className="flex items-center space-x-4 min-h-[90px] rounded-lg border border-[#101010] p-4 bg-[#101010]"
+                  variants={fadeIn}
                 >
                   <CheckCircle className="h-6 w-6 text-[#c3a540]" />
                   <span className="text-lg font-medium text-[#c3a540]">
                     {benefit}
                   </span>
-                </div>
+                </motion.div>
               ))}
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </section>
         <section
           id="cta"
           className="w-full flex justify-center py-12 md:py-24 lg:py-32 bg-[#101010]"
         >
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+          <motion.div
+            className="container px-4 md:px-6"
+            variants={staggerChildren}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+          >
+            <motion.div
+              className="flex flex-col items-center justify-center space-y-4 text-center"
+              variants={fadeIn}
+            >
               <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter text-[#c3a540] sm:text-5xl">
                   Pronto para Transformar seu Negócio?
                 </h2>
-                <p className="text-gray-300 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                <p className="max-w-[600px] text-gray-300 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   Adquira agora o ebook {'"Visionário de Sucesso"'} e comece sua
                   jornada rumo ao sucesso digital.
                 </p>
               </div>
-              <div className="w-full max-w-sm space-y-2">
+              <motion.div
+                className="w-full max-w-sm space-y-2"
+                variants={fadeIn}
+              >
                 <form className="flex flex-col gap-2">
                   <Input
                     className="bg-white text-[#101010]"
@@ -203,7 +265,7 @@ export default function LandingPage() {
                   />
                   <Button
                     className="bg-[#c3a540] text-[#101010] hover:bg-[#e0b038]"
-                    type="button"
+                    type="submit"
                   >
                     Comprar Ebook
                     <ChevronRight className="ml-2 h-4 w-4" />
@@ -213,32 +275,41 @@ export default function LandingPage() {
                   Ao comprar, você concorda com nossos{" "}
                   <Link
                     className="underline underline-offset-2 text-[#c3a540]"
-                    href="/politica-privacidade"
+                    href="/privacy"
                   >
                     Termos de Serviço
                   </Link>
                   .
                 </p>
-              </div>
-            </div>
-          </div>
+              </motion.div>
+            </motion.div>
+          </motion.div>
         </section>
       </main>
-      <footer className="py-6 w-full shrink-0 px-4 md:px-6 border-t border-[#c3a540] bg-[#101010]">
-        <div className="items-center flex flex-col mx-auto gap-2 sm:flex-row max-w-[1488px]">
-          <p className="text-xs text-gray-400">
-            © 2024 Visionário de Sucesso. Todos os direitos reservados.
-          </p>
-          <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-            <Link
-              className="text-xs hover:underline underline-offset-4 text-[#c3a540]"
-              href="/politica-privacidade"
-            >
-              Termos de Serviço
-            </Link>
-          </nav>
-        </div>
-      </footer>
+      <motion.footer
+        className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t border-[#c3a540] bg-[#101010]"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+      >
+        <p className="text-xs text-gray-400">
+          © 2024 Visionário de Sucesso. Todos os direitos reservados.
+        </p>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <Link
+            className="text-xs hover:underline underline-offset-4 text-[#c3a540]"
+            href="#"
+          >
+            Termos de Serviço
+          </Link>
+          <Link
+            className="text-xs hover:underline underline-offset-4 text-[#c3a540]"
+            href="/privacy"
+          >
+            Privacidade
+          </Link>
+        </nav>
+      </motion.footer>
     </div>
   );
 }
