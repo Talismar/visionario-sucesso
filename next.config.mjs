28cdc888ee0eb1fb4118b1450f0e775ab589b578
Talismar Fernandes Costa <talismar788.una@gmail.com>
2024-10-16 21:49:09 -0300

    fix: basePath url on next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV == 'production'
 const nextConfig = {
     output: "export",
-    basePath: "/nextjs-github-pages"
+    basePath: isProd ? "/visionario-sucesso" : ''
 };
 
 export default nextConfig;
